4342196937ad616462b05f5a38b0d9fc24c65c50
weida <liuweida1010@gmail.com>
2025-01-24 09:38:03 +0100

    完成后端v2.1 修复baseDo字段和数据库字段不符的问题

--- project/src/main/java/org/rhw/bmr/project/common/database/BaseDO.java
+++ project/src/main/java/org/rhw/bmr/project/common/database/BaseDO.java
@@ -8,17 +8,17 @@ import java.util.Date;
 
 @Data
 public class BaseDO {
-    @TableField(fill = FieldFill.INSERT)
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
     private Date createTime;
     /**
      * 修改时间
      */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @TableField(value = "update_time",fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
     /**
      * 删除表示，0：未删除 1：删除
      */
-    @TableField(fill = FieldFill.INSERT)
+    @TableField(value = "del_flag", fill = FieldFill.INSERT)
     private Integer delFlag;
     
 }
